96a31e347436e28d06cafac6193d66ece827ce8e
frozensox <frozensox@icloud.com>
2021-01-26 23:12:43 +0900

    install.shの誤りを修正

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ error_log() {
 }
 
 (docker-compose up -d --build || error_log 'Failed to build container.') &&
-(docker-compose exec web composer create-project --prefer-dist laravel/laravel . || error_log 'Failed to install Laravel.') &&
-(docker-compose exec web php artisan migrate || error_log 'Failed to migrate in Laravel.') &&
+(docker-compose exec app composer create-project --prefer-dist laravel/laravel . || error_log 'Failed to install Laravel.') &&
+(docker-compose exec app php artisan migrate || error_log 'Failed to migrate in Laravel.') &&
 echo '' &&
 echo 'Installation was successful.'
